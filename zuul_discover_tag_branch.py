4407b7a8190a284abf0da0de27ef9fbb655ebaa6
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-12 04:57:46 +0000

    discover_tag: revert to git clone...
    Change-Id: I020b79f8ba08c9c9a5f226555948f068c0380685

--- zuul_discover_tag_branch.py
+++ zuul_discover_tag_branch.py
@@ -32,7 +32,11 @@ class ZuulDiscoverTagBranch(zuul_koji_lib.App):
     def main(self, args):
         tag = args.ref.replace('refs/tags/', '')
         if not os.path.isdir(args.project):
-            self.execute(["zuul-cloner", args.git_server, args.project])
+            os.makedirs(args.project)
+            self.execute(["git", "clone", "-q",
+                          "%s/%s" % (args.git_server, args.project),
+                          args.project], capture=True)
+            self.execute(["git", "checkout", "-q", tag], cwd=args.project)
         self.log.info("Looking for branch containing ref %s" % tag)
         branches = self.execute(["git", "branch", "-a", "--contains", tag],
                                 capture=True, cwd=args.project).split('\n')
