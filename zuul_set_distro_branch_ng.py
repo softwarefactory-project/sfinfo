e17e881103179600fda6ce505d070e93a62c0a58
Fabien Boucher <fboucher@redhat.com>
2017-07-06 18:03:46 +0200

    WIP - zuul_set_distro_branch_ng
    Change-Id: Ic3d06cc5757a28a07e743162132ecf1222cf6cd3

---
+++ zuul_set_distro_branch_ng.py
@@ -0,0 +1,140 @@
+#!/bin/env python
+#
+# Copyright 2017 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import re
+import os
+import sys
+import pwd
+import yaml
+import argparse
+
+import zuul_koji_lib
+
+
+class ZuulSetDistroBranch(zuul_koji_lib.App):
+    def usage(self):
+        p = argparse.ArgumentParser(description='Zuul Set Distro Branch')
+        p.add_argument("--git-server", default=os.environ.get("GIT_SERVER",
+                       "https://softwarefactory-project.io/r"))
+        p.add_argument("--local-git-dir", default="~/koji-git")
+        p.add_argument("--project-resources", default="software-factory.yaml")
+        p.add_argument("--username", default=pwd.getpwuid(os.getuid())[0],
+                       help="Username used to git push")
+        return p
+
+    def clone_config_repo(self, base_dir, git_server):
+        dirname = "%s/%s" % (base_dir, 'config')
+        if not os.path.isdir(os.path.dirname(dirname)):
+            os.mkdir(os.path.dirname(dirname), 0700)
+        repourl = "%s/config" % git_server
+        # bfrom = self.distro_info.get("from", "master")
+        if not os.path.isdir(dirname):
+            self.log.info("Cloning %s to %s" % (repourl, dirname))
+            self.execute(["git", "clone", repourl, dirname])
+        else:
+            self.log.info("Fetch all references of %s" % dirname)
+            self.execute(["git", "fetch", "--all"], cwd=dirname)
+            self.log.info("Checkout branch master for repo config")
+            self.execute(["git", "reset", "--hard", "origin/master"],
+                         cwd=dirname)
+
+    def push_config_repo_review(self, base_dir, username):
+        remote = "ssh://%s@%s:%s/config" % (username, host, port)
+        self.log.info("Push review on repo config")
+        self.execute(["git", "push", remote, 'refs/for/master'],
+                     cwd=dirname)
+
+    def repo_get_offset(self, raw, name):
+        offset = 0
+        # Find repos sections
+        while True:
+            if 'repos:' in raw[offset]:
+                break
+            offset += 1
+        # Find repo definition
+        while True:
+            if '%s:' % name in raw[offset]:
+                break
+            offset += 1
+        start = offset
+        # Find the end of the repo definition section
+        # based on the indent
+        # Get indent offset
+        ioffset = raw[start].find('%s:' % name)
+        while True:
+            offset += 1
+            if offset >= len(raw):
+                 # EOF
+                 return start, len(raw), ioffset
+            # Remove indent
+            nextl = raw[offset].strip()
+            inextl = raw[offset].find(nextl)
+            if inextl == ioffset:
+                end = offset
+                break
+        return start, end, ioffset
+
+    def update_repo_def(self, resources, raw_resources, name, branch):
+        orepo = resources['resources']['repos'].get(name)
+        if not orepo:
+            self.log.info("Repo %s not found in the current resources file. Skip !" % name)
+            return
+        start, end, ioffset = self.repo_get_offset(
+                raw_resources, name)
+        # Remove old repo section
+        raw_resources[start:end] = []
+        repo = {}
+        repo[name] = orepo
+        repo[name].setdefault('branches', {})
+        repo[name]['branches'][branch] = 'HEAD'
+        repo_raw = yaml.dump(repo, default_flow_style = False).split('\n')
+        repo_raw = ['%s%s\n' % (" " * ioffset, l) for l in repo_raw]
+        del repo_raw[-1]
+        # Insert new repo section
+        raw_resources[start:start] = repo_raw
+
+    def main(self, args):
+        base_dir = os.path.expanduser(args.local_git_dir)
+        if not os.path.isdir(base_dir):
+            os.mkdir(base_dir, 0700)
+        # Get repos list to act on
+        packages = self.distro_info["packages"]
+        repos_names = []
+        for package in packages:
+            if package['source'] == 'internal':
+                repos_names.append(package['name'])
+                repos_names.append(package['distgit'])
+            else:
+                repos_names.append(package['distgit'])
+        # Get and load the resources file
+        self.clone_config_repo(base_dir, args.git_server)
+        p_resources_path = os.path.join(base_dir,
+            'config', 'resources', args.project_resources)
+        raw_resources = file(p_resources_path).readlines()
+        resources = yaml.load(file(p_resources_path))
+        for name in repos_names:
+            print name
+            self.update_repo_def(
+                resources,
+                raw_resources, 
+                name,
+                '2.6')
+        file('out', 'w').writelines(raw_resources)
+        #self.push(config, base_dir, args.username)
+
+
+if __name__ == "__main__":
+    ZuulSetDistroBranch()
