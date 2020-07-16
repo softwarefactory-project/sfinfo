2834d6015d93b7651a5f69ddd08f95c846b7092a
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-16 15:15:40 +0000

    Initial summon
    Change-Id: I4673e64db9ddfa60db09e6d570b16e6bd4224e4c

---
+++ src/Sfinfo.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Tristan De Cacqueray
+SPDX-License-Identifier: Apache-2.0
+Maintainer: Tristan De Cacqueray <tdecacqu@redhat.com>
+
+Software Factory Release Tools
+-}
+
+module Sfinfo
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
