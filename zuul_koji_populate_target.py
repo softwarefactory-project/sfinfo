3baf9d4083976b3ace1f8355e32c4a5bad17c8c9
Tristan Cacqueray <tdecacqu@redhat.com>
2021-02-15 17:09:38 +0000

    Improve rpms/python workaround
    Change-Id: Idfaeda7b896c49f379bce8fe4bf6c79ae7bdb1a0

--- zuul_koji_populate_target.py
+++ zuul_koji_populate_target.py
@@ -152,11 +152,10 @@ class ZuulKojiPopulateTarget(zuul_koji_lib.App):
         to_tag = []
         missing_packages = []
         for package in packages:
-            name = os.path.basename(package["name"])
+            name = os.path.basename(package["name"].replace(
+                "rpms/python-", "rpms/python3-"))
             if package.get("scl"):
                 name = "%s-%s" % (package["scl"], name)
-            if package["name"].startswith("rpms/python-"):
-                name = name.replace("python-", "python3-")
             if candidate and not package["valid_nvr"]:
                 missing_packages.append(name)
                 continue
